68097a10db83f02d898f69e182439c1473f8814c
Jianing Wang <24wangj@gmail.com>
2022-10-14 19:28:23 -0400

    Centered headers

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function toggleMenu() {
   }
 }
 
+function updateSidInfo() {
+  document.getElementById("sid-title").innerHTML = "Possible future employee";
+  document.getElementById("sid-info").innerHTML = "Hi! My name is Sid. I am a middle school graduate student currently attending Columbus North High School. I can bench over 45 pounds and I achieved silver honor roll in my 4th year at Saint Bartholomew School. I have a 4.33 GPA on a 12-point scale and plan on early decisioning to Ivy Tech Community College. My girlfriend is 11 years old, currently in fifth grade. Along with all of this, I was able to successfully get my driver's permit after only two attempts.";
+}
+
 window.addEventListener('resize', function(event) {
   if (window.innerWidth > 920) {
     document.getElementById('expand-contract').style.marginTop = '0px';
